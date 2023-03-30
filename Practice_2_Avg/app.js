6b1d812c9635ac8bdf930e848b85c118f88a7777
Jaga <jagblst@gmail.com>
2023-03-30 10:39:24 +0300

    Find the average function
    completed

--- Practice_2_Avg/app.js
+++ Practice_2_Avg/app.js
@@ -1,3 +1,7 @@
 // Write a function to find the average value in an array of numbers
-//avg([0,50]) //25
-//avg([75,76,80,95,100]) //85.2
+console.log(avg([0,50])) //25
+console.log(avg([75,76,80,95,100])) //85.2
+
+function avg(arr) {
+    return arr.reduce((a, b) => a + b) / arr.length;
+}
