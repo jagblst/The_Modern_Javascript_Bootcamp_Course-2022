082ab73ef75a0c544de284fe7f8114e5a35f6708
Jaga <jagblst@gmail.com>
2023-03-30 12:01:35 +0300

    Pangrams function
    completed

--- Practice_3_Pangrams/app.js
+++ Practice_3_Pangrams/app.js
@@ -3,5 +3,14 @@
 
 // Write a function called isPangram, which checks to see if a given sentence contains every letter of the alphabet.  Make sure you igore string casing!
 
-// isPangram('The five boxing wizards jump quickly') //true
-// isPangram('The five boxing wizards jump quick') //false
+console.log(isPangram('The five boxing wizards jump quickly')) //true
+console.log(isPangram('The five boxing wizards jump quick')) //false
+
+
+function isPangram(str) {
+    for (let char of 'abcdefghijklmnopqrstuvwxyz'){
+      if (str.toLowerCase().indexOf(char) === -1 )
+      return false
+    }
+  return true
+}
