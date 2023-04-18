2d1f4ea6a6d98703f1ebf4c783f022a6222d3edb
Jaga <jagblst@gmail.com>
2023-04-18 17:06:21 +0300

    Maze Game
    Grid Generation
    Verticals and Horizontals

--- Maze/index.js
+++ Maze/index.js
@@ -1,12 +1,8 @@
-const {
-  Engine,
-  Render,
-  Runner,
-  World,
-  Bodies,
-  MouseConstraint,
-  Mouse
-} = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
+
+const cells = 5;
+const width = 600;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -14,27 +10,33 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600
+    wireframes: true,
+    width,
+    height
   }
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-  world,
-  MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-  })
-);
-
 // Walls
 const walls = [
-  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-  Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
 ];
 World.add(world, walls);
 
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+// Maze generation
+
+const grid = Array(cells)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+  .fill(null)
+  .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
