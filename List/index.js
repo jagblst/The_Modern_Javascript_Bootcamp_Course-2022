8b9ae15c7c30987b532ae043f1330d4ab12f90ad
Jaga <jagblst@gmail.com>
2023-04-21 16:17:18 +0300

    Create Node JS  Command Line Tools
    A Callback-Based Solution

--- List/index.js
+++ List/index.js
@@ -7,15 +7,27 @@ fs.readdir(process.cwd(), (err, filenames) => {
     console.log(err);
   }
 
-  // BAD CODE HERE!!!!!
+  const allStats = Array(filenames.length).fill(null);
+
   for (let filename of filenames) {
+    const index = filenames.indexOf(filename);
+
     fs.lstat(filename, (err, stats) => {
       if (err) {
         console.log(err);
       }
 
-      console.log(filename, stats.isFile());
+      allStats[index] = stats;
+
+      const ready = allStats.every(stats => {
+        return stats;
+      });
+
+      if (ready) {
+        allStats.forEach((stats, index) => {
+          console.log(filenames[index], stats.isFile());
+        });
+      }
     });
   }
-  // BAD CODE COMPLETE
 });
