5c389e733d28bc27f8f75cbf161761f9771f9146
Jaga <jagblst@gmail.com>
2023-05-01 14:05:10 +0300

    Ecommerce_website
    Password Validation

--- ecomm_website/routes/admin/auth.js
+++ ecomm_website/routes/admin/auth.js
@@ -7,7 +7,9 @@ const signinTemplate = require('../../views/admin/auth/signin');
 const {
   requireEmail,
   requirePassword,
-  requirePasswordConfirmation
+  requirePasswordConfirmation,
+  requireEmailExists,
+  requireValidPasswordForUser
 } = require('./validators');
 
 const router = express.Router();
@@ -21,7 +23,10 @@ router.post(
   [requireEmail, requirePassword, requirePasswordConfirmation],
   async (req, res) => {
     const errors = validationResult(req);
-    console.log(errors);
+
+    if (!errors.isEmpty()) {
+      return res.send(signupTemplate({ req, errors }));
+    }
 
     const { email, password, passwordConfirmation } = req.body;
     const user = await usersRepo.create({ email, password });
@@ -41,26 +46,21 @@ router.get('/signin', (req, res) => {
   res.send(signinTemplate());
 });
 
-router.post('/signin', async (req, res) => {
-  const { email, password } = req.body;
+router.post(
+  '/signin',
+  [requireEmailExists, requireValidPasswordForUser],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
 
-  const user = await usersRepo.getOneBy({ email });
+    const { email } = req.body;
 
-  if (!user) {
-    return res.send('Email not found');
+    const user = await usersRepo.getOneBy({ email });
+
+    req.session.userId = user.id;
+
+    res.send('You are signed in!!!');
   }
-
-  const validPassword = await usersRepo.comparePasswords(
-    user.password,
-    password
-  );
-  if (!validPassword) {
-    return res.send('Invalid password');
-  }
-
-  req.session.userId = user.id;
-
-  res.send('You are signed in!!!');
-});
+);
 
 module.exports = router;
--- ecomm_website/routes/admin/validators.js
+++ ecomm_website/routes/admin/validators.js
@@ -1,5 +1,5 @@
 const { check } = require('express-validator');
-const usersRepo = require('../../repositories');
+const usersRepo = require('../../repositories/users');
 
 module.exports = {
   requireEmail: check('email')
@@ -24,6 +24,33 @@ module.exports = {
     .custom((passwordConfirmation, { req }) => {
       if (passwordConfirmation !== req.body.password) {
         throw new Error('Passwords must match');
+      }
+    }),
+  requireEmailExists: check('email')
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage('Must provide a valid email')
+    .custom(async email => {
+      const user = await usersRepo.getOneBy({ email });
+      if (!user) {
+        throw new Error('Email not found!');
+      }
+    }),
+  requireValidPasswordForUser: check('password')
+    .trim()
+    .custom(async (password, { req }) => {
+      const user = await usersRepo.getOneBy({ email: req.body.email });
+      if (!user) {
+        throw new Error('Invalid password');
+      }
+
+      const validPassword = await usersRepo.comparePasswords(
+        user.password,
+        password
+      );
+      if (!validPassword) {
+        throw new Error('Invalid password');
       }
     })
 };
