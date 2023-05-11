44d7198c32ab1b986db90074e0af86fe261073c0
Jaga <jagblst@gmail.com>
2023-05-11 12:47:17 +0300

    Movies-testing
    Writing Assertions
    Fake DOM Events
    Holding Up Assertions

--- movies-testing/test/autocomplete.test.js
+++ movies-testing/test/autocomplete.test.js
@@ -1,4 +1,5 @@
-it('Shows an autocomplete', () => {
+beforeEach(() => {
+  document.querySelector('#target').innerHTML = '';
   createAutoComplete({
     root: document.querySelector('#target'),
     fetchData() {
@@ -12,4 +13,21 @@ it('Shows an autocomplete', () => {
       return movie.Title;
     }
   });
+});
+
+it('Dropdown starts closed', () => {
+  const dropdown = document.querySelector('.dropdown');
+
+  expect(dropdown.className).not.to.include('is-active');
+});
+
+it('After searching, dropdown opens up', () => {
+  const input = document.querySelector('input');
+  input.value = 'avengers';
+  input.dispatchEvent(new Event('input'));
+
+  await waitFor('.dropdown-item');
+  
+  const dropdown = document.querySelector('.dropdown');
+  expect(dropdown.className).to.include('is-active');
 });
