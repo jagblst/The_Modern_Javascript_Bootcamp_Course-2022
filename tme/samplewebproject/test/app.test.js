2260a8f83b1331d24a61ac0b0aafc5657b8afac3
Jaga <jagblst@gmail.com>
2023-05-16 11:59:41 +0300

    Test_Me
    Script Execution Delay
    Implementing a Delay

--- tme/samplewebproject/test/app.test.js
+++ tme/samplewebproject/test/app.test.js
@@ -8,7 +8,7 @@ it('has a text input', async () => {
   assert(input);
 });
 
-it('shows a success message with a valid emai', async () => {
+it('shows a success message with a valid email', async () => {
   const dom = await render('index.html');
 
   const input = dom.window.document.querySelector('input');
@@ -19,5 +19,19 @@ it('shows a success message with a valid emai', async () => {
 
   const h1 = dom.window.document.querySelector('h1');
 
-  console.log('Contents of h1', h1.innerHTML);
+  assert.strictEqual(h1.innerHTML, 'Looks good!');
+});
+
+it('shows a fail message with a invalid email', async () => {
+  const dom = await render('index.html');
+
+  const input = dom.window.document.querySelector('input');
+  input.value = 'alskdj';
+  dom.window.document
+    .querySelector('form')
+    .dispatchEvent(new dom.window.Event('submit'));
+
+  const h1 = dom.window.document.querySelector('h1');
+
+  assert.strictEqual(h1.innerHTML, 'Invalid email');
 });
